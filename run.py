406068f624c2ecb7ca8cdd8fc3ff649d674887ea
Shivansh Srivastava <srivastavashiv0112@gmail.com>
2022-12-22 03:43:43 +0530

    yolov5

---
+++ run.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python ./yolov5/detect.py --weights ./yolov5/best.pt --img 416 --conf 0.7 --source 0")
---
+++ yolov5/run.py
@@ -0,0 +1,3 @@
+import os
+
+os.system("python detect.py --weights best.pt --img 416 --conf 0.7 --source ../demo.mp4")
